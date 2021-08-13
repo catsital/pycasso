be8c164b29c9b5e0f911695ed994d57ded29e864
catsital <catshital@gmail.com>
2021-08-13 22:35:35 +0800

    Add src files

---
+++ pycasso.py
@@ -0,0 +1,86 @@
+import math
+import shuffleseed
+import PIL.Image
+
+class Pycasso:
+    def __init__(self, img, slice_size, seed, output):
+        self.img = PIL.Image.open(img)
+        self.slice_size = slice_size
+        self.seed = seed
+        self.output = output
+        self.imgwidth, self.imgheight = self.img.size
+        self.total_parts = math.ceil(self.imgwidth/self.slice_size) * math.ceil(self.imgheight/self.slice_size)
+        self.canvas = PIL.Image.new(mode="RGBA", size=(self.imgwidth, self.imgheight), color=(255,255,255))
+
+    def get_slices(self):
+        slices = {}
+        vertical_slices = math.ceil(self.imgwidth/self.slice_size)
+        horizontal_slices = self.imgheight/self.slice_size
+        for i in range(0, self.total_parts):
+            slice = {}
+            row = int(i/vertical_slices)
+            col = i-row*vertical_slices
+            slice['x'] = col*self.slice_size
+            slice['y'] = row*self.slice_size
+            slice['width'] = (self.slice_size-(0 if slice['x']+self.slice_size<=self.imgwidth else (slice['x']+self.slice_size)-self.imgwidth))
+            slice['height'] = (self.slice_size-(0 if slice['y']+self.slice_size<=self.imgheight else (slice['y']+self.slice_size)-self.imgheight))
+            if str(slice['width'])+"-"+str(slice['height']) not in slices.keys():
+                slices[str(slice['width'])+"-"+str(slice['height'])] = []
+            slices[str(slice['width'])+"-"+str(slice['height'])].append(slice)
+
+        return slices
+
+    def get_cols_in_group(self, slices):
+        if len(slices)==1:
+            return 1
+        t = 'init'
+        for i in range(0, len(slices)):
+            if t == 'init':
+                t = slices[i]['y']
+            if not t == slices[i]['y']:
+                return i
+                break
+        return i
+
+    def get_group(self, slices):
+        this = {}
+        this['slices'] = len(slices)
+        this['cols'] = self.get_cols_in_group(slices)
+        this['rows'] = len(slices)/this['cols']
+        this['width'] = slices[0]['width']*this['cols']
+        this['height'] = slices[0]['height']*this['rows']
+        this['x'] = slices[0]['x']
+        this['y'] = slices[0]['y']
+        return this
+
+    def export(self, mode='scramble'):
+        slices = self.get_slices()
+
+        for g in slices:
+            group = self.get_group(slices[g])
+            shuffle_ind = []
+            for i in range(0, len(slices[g])):
+                shuffle_ind.append(i)
+            if mode == 'unscramble':
+                shuffle_ind = shuffleseed.unshuffle(shuffle_ind, self.seed)
+            if mode == 'scramble':
+                shuffle_ind = shuffleseed.shuffle(shuffle_ind, self.seed)
+
+            for i in range(0, len(slices[g])):
+                s = shuffle_ind[i]
+                row = int(s/group['cols'])
+                col = s-row*group['cols']
+                x = col*slices[g][i]['width']
+                y = row*slices[g][i]['height']
+
+                tiles = self.img.crop(box=(group['x']+x,
+                                           group['y']+y,
+                                           group['x']+x+slices[g][i]['width'],
+                                           group['y']+y+slices[g][i]['height']))
+
+                self.canvas.paste(tiles, box=(slices[g][i]['x'],
+                                              slices[g][i]['y'],
+                                              slices[g][i]['x']+slices[g][i]['width'],
+                                              slices[g][i]['y']+slices[g][i]['height']))
+
+        self.canvas.save(self.output + ".png")
---
+++ shuffleseed.py
@@ -0,0 +1,65 @@
+import math
+import random
+
+def seedrandom(seed):
+    random.seed(seed)
+    seed = random.random()
+    return seed
+
+def isarray(this):
+    return type(this) is list
+
+def seedrand(seed, min, max):
+    return math.floor(seed * (max - min + 1)) + min
+
+def shuffle(arr, seed=None):
+    if not isarray(arr):
+        return None
+
+    size = len(arr)
+    rng = seedrandom(seed)
+    resp = []
+    keys = []
+
+    for i in range(0, size):
+        keys.append(i)
+
+    for i in range(0, size):
+        r = seedrand(rng, 0, len(keys)-1)
+        g = keys[r]
+        splice(keys, r, 1) # keys.insert(r, keys.pop())
+        resp.append(arr[g])
+
+    return resp
+
+def unshuffle(arr, seed=None):
+    if not isarray(arr):
+        return None
+
+    size = len(arr)
+    rng = seedrandom(seed)
+    resp = []
+    map = []
+    keys = []
+
+    for i in range(0, size):
+        resp.append(None)
+        keys.append(i)
+
+    for i in range(0, size):
+        r = seedrand(rng, 0, len(keys)-1)
+        g = keys[r]
+        splice(keys, r, 1)
+        resp[g] = arr[i]
+
+    return resp
+
+def splice(target, start, delete_count=None, *items):
+    if delete_count == None:
+        delete_count = len(target) - start
+
+    total = start + delete_count
+    removed = target[start:total]
+    target[start:total] = items
+
+    return removed
