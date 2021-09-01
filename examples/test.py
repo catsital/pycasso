364e685a0c50f5e69b7be853138c20716fc1cbd5
catsital <catshital@gmail.com>
2021-09-02 05:30:12 +0800

    Add/update tests

--- examples/test.py
+++ examples/test.py
@@ -6,4 +6,10 @@ seed = 'Pycasso'
 pyc = Canvas(img, slice_size, seed, 'en_Pepper-and-Carrot_by-David-Revoy_E05P01_p2_scramble')
 pyc.export(mode='scramble')
 
-# Canvas('examples/en_Pepper-and-Carrot_by-David-Revoy_E05P01_p2.png', 50, 'Pycasso', 'en_Pepper-and-Carrot_by-David-Revoy_E05P01_p2_scramble-test').export('scramble')
+# Canvas('examples/en_Pepper-and-Carrot_by-David-Revoy_E05P01_p2.png', 30, 'Pycasso', 'en_Pepper-and-Carrot_by-David-Revoy_E05P01_p2_scramble').export('scramble')
+
+img = 'examples/en_Pepper-and-Carrot_by-David-Revoy_E05P01_p2_scramble.png'
+pyc = Canvas(img, slice_size, seed, 'en_Pepper-and-Carrot_by-David-Revoy_E05P01_p2_unscramble')
+pyc.export(mode='unscramble')
+
+# Canvas('examples/en_Pepper-and-Carrot_by-David-Revoy_E05P01_p2_scramble.png', 30, 'Pycasso', 'en_Pepper-and-Carrot_by-David-Revoy_E05P01_p2_unscramble').export('unscramble')
