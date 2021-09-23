2bedaff4f7264951f593404b944ca7ca311460f7
catsital <catshital@gmail.com>
2021-09-23 21:54:32 +0800

    Reorganize project, clean up redundant files

---
+++ pycasso/__init__.py
@@ -0,0 +1,5 @@
+__project__ = "pycasso"
+__version__ = "0.1.1"
+
+from .unscramble import Canvas
+from .shuffleseed import shuffle, unshuffle
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+requirements = ["Pillow>=7.2.0"]
+
+setup(
+    name="pycasso",
+    version="0.1.1",
+    author="catsital",
+    author_email="catshital@gmail.com",
+    description="Split image into tiles and scramble/unscramble it with seed.",
+    install_requires=requirements,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/catsital/pycasso",
+    project_urls={
+        "Bug Tracker": "https://github.com/catsital/pycasso/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    zip_safe=False
+)
